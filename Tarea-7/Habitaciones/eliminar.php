713685a30cdf17eacf55a68feacacbcbfd977592
santinaba <santinavarrobalanza@gmail.com>
2019-10-02 00:23:32 -0400

    Tarea7

--- Tarea-7/Habitaciones/eliminar.php
+++ Tarea-7/Habitaciones/eliminar.php
@@ -1,7 +1,7 @@
 <?php  session_start();
 if (isset($_SESSION['nombre']) and $_SESSION['nivel']==1 )
 {
-include('../conexion.php');
+include('conexion.php');
 $id=$_GET["id"];
 $sql="DELETE FROM  tipohabitacion WHERE id=$id";
 $con->query($sql);
