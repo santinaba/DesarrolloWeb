719d3fe73629909014178a3d81a8183e435daffd
santinaba <santinavarrobalanza@gmail.com>
2019-10-15 13:44:15 -0400

    laboratio3

--- Tarea-7/Habitaciones/insertar.php
+++
@@ -1,9 +0,0 @@
-<?php include('../conexion.php');
-$habitacion=$_POST["txtHabitacion"];
-$precio=$_POST["txtPrecio"];
-$sql="INSERT INTO tipohabitacion(habitacion,precio) values ('$habitacion',$precio)";
-$con->query($sql);
-if ($con->connect_error)
-	echo "no se pudo insertar".$con->connect_error;
- ?>
- <meta http-equiv="refresh" content="1;URL=habitaciones.php">
--- Tarea-7/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$server="localhost";
-	$user="root";
-	$pass="";
-	$bd="bd_hotel1";
-	$con=new mysqli($server,$user,$pass,$bd);
-	if ($con->connect_error) die("Conexion Fallada ".$con->connect_error);
-?>
