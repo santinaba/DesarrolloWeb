99ad34991336da9ca1a1a2ff32efa28b184d62be
santinaba <santinavarrobalanza@gmail.com>
2019-11-25 13:34:59 -0400

    tarea7

--- tarea-7/Ahorcado/ahorcado.js
+++ tarea-7/Ahorcado/ahorcado.js
@@ -1,41 +1,23 @@
-// ### VARIABLES ###
-
-// Array de palabras
-var palabras = [["atlantico", "Un océano"], ["ordenador", "Una máquina"], ["laurel", "Un árbol"], ["plaza", "Espacio público"], ["rueda", "Gran invento"], ["cereza", "Una fruta"], ["petanca", "Un juego"], ["higuera", "Un árbol"], ["everest", "Un monte"], ["relampago", "Antecede al trueno"], ["jirafa", "Un animal"], ["luxemburgo", "Un país"], ["uruguay", "Un país"], ["ilustracion", "Representación gráfica"], ["excursion", "Actividad en la naturaleza"], ["empanadilla", "De la panadería"], ["pastel", "De la pastelería"], ["colegio", "Lugar para estudiar"], ["carrera", "Competición"], ["mermelada", "Confitura"]];
-// Palabra a averiguar
+var palabras = [["pacifico", "maremoto"], ["computador", "escritura"], ["oregano", "arbusto"], ["parque", "eepacio"], ["llanta", "invencion"], ["frambuesa", "manzana"], ["petanca", "juego"], ["higuera", "arboleda"], ["everest", "montaña"], ["relampago", "Antecede al trueno"], ["jirafa", "Un animal"], ["luxemburgo", "Un país"], ["uruguay", "Un país"], ["ilustracion", "Representación gráfica"], ["excursion", "Actividad en la naturaleza"], ["empanadilla", "De la panadería"], ["pastel", "De la pastelería"], ["colegio", "Lugar para estudiar"], ["carrera", "Competición"], ["mermelada", "Confitura"]];
 var palabra = "";
-// Nº aleatorio
 var rand;
-// Palabra oculta
 var oculta = [];
-// Elemento html de la palabra
 var hueco = document.getElementById("palabra");
-// Contador de intentos
 var cont = 6;
-// Botones de letras
 var buttons = document.getElementsByClassName('letra');
-// Boton de reset
 var btnInicio = document.getElementById("reset");
 
-
-// ### FUNCIONES ###
-
-// Escoger palabra al azar
 function generaPalabra() {
   rand = (Math.random() * 19).toFixed(0);
   palabra = palabras[rand][0].toUpperCase();
   console.log(palabra);
 }
-
-// Funcion para pintar los guiones de la palabra
 function pintarGuiones(num) {
   for (var i = 0; i < num; i++) {
     oculta[i] = "_";
   }
   hueco.innerHTML = oculta.join("");
 }
-
-//Generar abecedario
 function generaABC (a,z) {
   document.getElementById("abcdario").innerHTML = "";
   var i = a.charCodeAt(0), j = z.charCodeAt(0);
@@ -48,8 +30,6 @@ function generaABC (a,z) {
     }
   }
 }
-
-// Chequear intento
 function intento(letra) {
   document.getElementById(letra).disabled = true;
   if(palabra.indexOf(letra) != -1) {
@@ -71,13 +51,9 @@ function intento(letra) {
     document.getElementById("acierto").className = ""; 
   }, 800);
 }
-
-// Obtener pista
 function pista() {
   document.getElementById("hueco-pista").innerHTML = palabras[rand][1];
 }
-
-// Compruba si ha finalizado
 function compruebaFin() {
   if( oculta.indexOf("_") == -1 ) {
     document.getElementById("msg-final").innerHTML = "Felicidades !!";
@@ -98,8 +74,6 @@ function compruebaFin() {
     btnInicio.onclick = function () { location.reload() };
   }
 }
-
-// Restablecer juego
 function inicio() {
   generaPalabra();
   pintarGuiones(palabra.length);
@@ -107,6 +81,4 @@ function inicio() {
   cont = 6;
   document.getElementById("intentos").innerHTML=cont;
 }
-
-// Iniciar
 window.onload = inicio();
